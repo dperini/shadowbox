453b9575e490644f2c2f8b87e027f1f5879171c0
Diego Perini <diego.perini@gmail.com>
2011-05-23 15:57:13 +0200

    added initialization to setup event delegation

---
+++ source/init.js
@@ -0,0 +1,27 @@
+// solves cross browser needs
+document.onclick = function(e) {
+
+    e || ((this.ownerDocument || this.document || this).parentWindow || window).event;
+
+    var target = e.target || e.srcElement;
+
+    if (!initialized) S.skin.init();
+
+    while (target && target.nodeType == 1) {
+        if (target.nodeName.toUpperCase() == 'A' &&
+            /^(light|shadow)box(\[(.*)\])?/.test(target.getAttribute('rel'))) {
+
+            S.open(target);
+
+            if (S.gallery.length) {
+                if (e.preventDefault) {
+                    e.preventDefault();
+                } else {
+                    e.returnValue = false;
+                }
+                return false;
+            }
+        }
+        target = target.parentNode;
+    }
+};
